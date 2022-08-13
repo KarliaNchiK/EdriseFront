e3c73688af174cd77e2bc2860f9b5f811391bb31
Ainur Fattakhov <karlianchik@gmail.com>
2022-08-13 23:34:17 +0300

    set edrise

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: 'frontNuxt',
+        name: 'edrise',
         exec_mode: 'cluster',
         instances: 'max',
         script: './node_modules/nuxt/bin/nuxt.js',
